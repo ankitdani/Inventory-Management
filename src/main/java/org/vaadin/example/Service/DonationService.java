b6f1e0b4b198dcef7542adba5f2065cdcdfcd513
Ankit Dani <ankitdani1997@gmail.com>
2024-02-02 21:59:32 -0800

    Added DonationService class

--- src/main/java/org/vaadin/example/Service/DonationService.java
+++ src/main/java/org/vaadin/example/Service/DonationService.java
@@ -1,5 +1,26 @@
 package org.vaadin.example.Service;
 
-public class DonationService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.vaadin.example.Entity.Donor;
+import org.vaadin.example.Repository.DonorRepository;
 
+import java.util.List;
+
+@Service
+public class DonationService {
+    private DonorRepository donorRepository;
+
+    @Autowired
+    public DonationService(DonorRepository donorRepository) {
+        this.donorRepository = donorRepository;
+    }
+
+    public List<Donor> getAllDonors(){
+        return donorRepository.findAll();
+    }
+
+    public Donor saveDonor(Donor donor){
+        return donorRepository.save(donor);
+    }
 }
