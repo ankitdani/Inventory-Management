2bde2adbd04068e4b8ba996008715c693bd57aa6
Ankit Dani <ankitdani1997@gmail.com>
2024-02-02 21:48:37 -0800

    Deleted donor details file

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -2,26 +2,21 @@ package org.vaadin.example;
 
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-
-import java.util.List;
-import java.util.Locale;
+import org.vaadin.example.Repository.DonorRepository;
 
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private final DonorRepository donorRepository;
+
+    public MainView(DonorRepository donorRepository) {
+        this.donorRepository = donorRepository;
         //grid of transactions
         Grid<DonorDetails> grid = new Grid<>(DonorDetails.class);
         grid.addColumn(DonorDetails::getDonorName);
