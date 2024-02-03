8aeeb4cf99fc451910e24ff7fc2a7b2928a4b58d
Ankit Dani <ankitdani1997@gmail.com>
2024-02-02 22:06:35 -0800

    Vaadin UI cleanout

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -10,74 +10,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.vaadin.example.Repository.DonorRepository;
 
-@Route
+@Route()
 public class MainView extends VerticalLayout {
-
-    private final DonorRepository donorRepository;
-
-    public MainView(DonorRepository donorRepository) {
-        this.donorRepository = donorRepository;
-        //grid of transactions
-        Grid<DonorDetails> grid = new Grid<>(DonorDetails.class);
-        grid.addColumn(DonorDetails::getDonorName);
-        grid.addColumn(DonorDetails::getDonationType);
-        grid.addColumn(DonorDetails::getQuantity);
-        grid.addColumn(DonorDetails::getDate);
-
-//        grid.setItems(donor);
-
-        TextField DonorName = new TextField();
-        DonorName.setLabel("Donor Name:");
-//        DonorName.setValue("FirstName LastName");
-        DonorName.setClearButtonVisible(true);
-//        DonorName.setPrefixComponent(VaadinIcon.MAP_MARKER.create());
-
-
-        TextField DonationType = new TextField();
-        DonationType.setLabel("Donation Type:");
-//        DonationType.setValue("Money, food, clothing");
-        DonationType.setClearButtonVisible(true);
-//        DonationType.setPrefixComponent(VaadinIcon.MAP_MARKER.create());
-
-
-        TextField Quantity = new TextField();
-        Quantity.setLabel("Quantity:");
-//        Quantity.setValue("$");
-        Quantity.setClearButtonVisible(true);
-//        Quantity.setPrefixComponent(VaadinIcon.MAP_MARKER.create());
-
-
-        DatePicker DateOfDonation = new DatePicker("Date of Donation:");
-        DateOfDonation.setAutoOpen(true);
-
-
-        Button Donate = new Button("Donate");
-        Donate.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-
-        Button Distribute = new Button("Distribute");
-        Distribute.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        Button AllDonations = new Button("View all Donations");
-        AllDonations.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        Button AllDistributions = new Button("View all Distributions");
-        AllDistributions.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-
-        VerticalLayout layout = new VerticalLayout();
-        layout.add(DonorName);
-        layout.add(DonationType);
-        layout.add(Quantity);
-        layout.add(DateOfDonation);
-        layout.add(Donate);
-        layout.add(Distribute);
-        layout.add(AllDonations);
-        layout.add(AllDistributions);
-        add(grid);
-        add(layout);
-
-
-    }
 
 }
